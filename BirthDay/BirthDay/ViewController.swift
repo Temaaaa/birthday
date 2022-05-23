88980d1428fa9a8ce747be98c29866c22e7f12a8
Artem Ledchak <61849711+Temaaaa@users.noreply.github.com>
2022-05-23 16:23:04 +0300

    Add files via upload

---
+++ BirthDay/BirthDay/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  BirthDay
+//
+//  Created by  Артем Ледчак on 23.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var birthdayList: [String] = []
+
+    
+    
+    @IBAction func AddPeaple(_ sender: Any) {
+        
+        
+        
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
